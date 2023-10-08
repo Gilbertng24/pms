8334bc2214bf2e73feeb3c3cec49860f4036a7f5
Gilbert Ng <gilbertng24@gmail.com>
2023-10-08 00:45:10 -0700

    Add axios api call

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from "axios";
+import NewsHeading from "./NewsHeading";
+import WithListLoading from "./WithListLoading";
+import List from "./List";
 
 function Home(){
+  const [appState, setAppState] = useState({
+    loading: false,
+    news: null,
+  });
+
+  const ListLoading = WithListLoading(List);
+
+  useEffect(() => {
+    async function fetchData() {
+      setAppState({ loading: true });
+      const apiUrl = 'https://newsapi.org/v2/top-headlines?country=ca&apiKey=c54af98eb3d54b599abd450a6d9ce599';
+      //const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+      const res = await axios.get(apiUrl);
+      //console.log(res.data.articles.slice(0,4))
+      //console.log(res)
+      setAppState({ loading: false, news: res.data.articles.slice(0,4) })
+    }
+    fetchData();
+  }, [setAppState])
+
   return (
     <div>
-      <h1>This is the home page!</h1>
+      <ListLoading isLoading={appState.loading} news={appState.news} />
     </div>
   )
 }
---
+++ src/components/List.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+
+const List = (props) => {
+  const { news } = props;
+
+
+  if (!news || news.length === 0) return <p>No todos, sorry</p>;
+  return (
+    <div className="container-fluid position-absolute bottom-0 start-0 bg-primary">
+        <footer className="text-start text-lg-start text-white">
+            {news.map((news) => {
+              return (
+                <div className="text-left p-3" key={nanoid()}>
+                      <span className="fw-bold">{news.author}:</span>
+                      <span> {news.title}</span>
+                </div>
+              );
+            })}
+        </footer>
+    </div>    
+  );
+};
+export default List;
